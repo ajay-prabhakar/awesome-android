7c46c4c3115f11d2e31c95242b6f372d13555729
prabhakar369 <ajayprabhakar369@gmail.com>
2019-02-26 19:34:26 +0530

    practice set 2

---
+++ app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android.practiceset2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        display1("This is Box 1.");
+        display2("And this is Box 2.");
+        display3("And look! Box 3!");
+
+    }
+
+    /**
+     * Display methods that allow the text to appear on the screen. Don't worry if you don't know
+     * how these work yet. We'll be covering them in lesson 3.
+     */
+
+    public void display(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text);
+    }
+
+    public void display(int text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(text + "");
+    }
+
+    public void display1(String text) {
+        display(text);
+    }
+
+    public void display2(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_2);
+        t.setText(text);
+    }
+
+    public void display3(String text) {
+        TextView t = (TextView) findViewById(R.id.display_text_view_3);
+        t.setText(text);
+    }
+}
